559adaf56bdf1036e8d1f3eaf9de96eb8f5d794b
Fletcher Ramee <fletcher.ramee@stonybrook.edu>
2021-10-06 21:00:28 -0400

    Remove data sources

--- index.js
+++ index.js
@@ -2,18 +2,12 @@ const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 require('dotenv').config();  // Loads environment variables from .env into process.env
 const { MONGO_URI } = process.env;  // Loads environment variables
-
 const { typeDefs } = require('./typedefs/root-def');
 const resolvers = require('./resolvers/root-resolvers');
-const User = require('./models/user-model.js');
-const Users = require('./datasources/users-datasource.js');
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    dataSources: (() => ({
-        users: new Users(User)
-    })),
     introspection: true
 });
 
--- resolvers/user-resolvers.js
+++ resolvers/user-resolvers.js
@@ -1,5 +1,6 @@
 
 const ObjectId = require('mongoose').Types.ObjectId;
+const User = require('../models/user-model.js');
 const { OAuth2Client } = require('google-auth-library');
 const { CLIENT_ID } = process.env;
 const client = new OAuth2Client(CLIENT_ID);
@@ -8,22 +9,21 @@ const client = new OAuth2Client(CLIENT_ID);
 // See https://www.apollographql.com/docs/apollo-server/data/resolvers/#resolver-arguments
 module.exports = {
     Query: {
-        getNumber: () => {}
+        getNumber: () => { return -1; }
     },
     Mutation: {
         login: async (_, { idToken }, context) => {
             // check if ID is already stored in users
             // if not, create new user with the ID
-            const users = context.dataSources.users;
             const ticket = await client.verifyIdToken({
                 idToken: idToken,
                 audience: CLIENT_ID
             });
             const { sub: googleId } = ticket.getPayload();
-            const user = await users.findUserByGoogleId(googleId);  // should be null if no document found
+            const user = await User.findUserByGoogleId(googleId);  // should be null if no document found
             if (!user) {
                 // new user -> create user document and login.
-                user = await users.createNewUser(googleId);
+                user = await User.createNewUser(googleId);
             }
             context.googleId = googleId;
             return user;
