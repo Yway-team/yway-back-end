2d847b0d34773d901e5146d886021fbba5cdd8ed
Fletcher Ramee <fletcher.ramee@stonybrook.edu>
2021-10-20 21:18:18 -0400

    Temporarily remove cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ async function startServer() {
     const { ApolloServer } = require('apollo-server-express');
     const express = require('express');
     
-    const cors = require('cors');
+    // const cors = require('cors');
     const helmet = require('helmet');
     const mongoSanitize = require('express-mongo-sanitize');
     const xssClean = require('xss-clean');
@@ -22,7 +22,7 @@ async function startServer() {
 
     await server.start();
 
-    app.use(cors({ origin: CLIENT_ORIGIN, credentials: true }));
+    // app.use(cors({ origin: CLIENT_ORIGIN, credentials: true }));  // Doesn't currently work - blocks Apollo Studio
     app.use(helmet({ contentSecurityPolicy: false }));  // may be insecure
     app.use(express.json());  // parses JSON
     app.use(express.urlencoded({ extended: false }));
@@ -31,9 +31,7 @@ async function startServer() {
 
     server.applyMiddleware({ app });
 
-    await new Promise(resolve => app.listen({ port: BACKEND_PORT }, resolve));
-
-    console.log(`🚀 Server ready at http://localhost:${BACKEND_PORT}${server.graphqlPath}`);
+    app.listen({ port: BACKEND_PORT }, () => { console.log(`🚀 Server ready at http://localhost:${BACKEND_PORT}${server.graphqlPath}`); })
 }
 
 mongoose.connect(MONGO_URI)
