d0a1c7a3ceea6fc30d097736b621f4f190944e79
John Park <loopuntiltrue@gmail.com>
2021-11-12 21:11:06 -0500

    Changes to enable backend HTTPS

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const { MONGO_URI, BACKEND_PORT, CLIENT_ORIGIN } = process.env;  // Loads enviro
 async function startServer() {
     const { ApolloServer } = require('apollo-server-express');
     const express = require('express');
+
+    const fs = require("fs")
+    const https = require("https")
+    const http = require("http")
     
     const helmet = require('helmet');
     const mongoSanitize = require('express-mongo-sanitize');
@@ -13,8 +17,17 @@ async function startServer() {
     
     const { typeDefs } = require('./typedefs/root-def');
     const resolvers = require('./resolvers/root-resolvers');
-    const corsPolicy = { origin: [CLIENT_ORIGIN, 'http://yway.app.s3-website.us-east-2.amazonaws.com', 'http://localhost:3000', 'https://studio.apollographql.com'], credentials: true };
+    const corsPolicy = { origin: [CLIENT_ORIGIN, 'http://yway.app.s3-website.us-east-2.amazonaws.com', 'http://localhost:3000', 'https://www.yway.app', 'https://studio.apollographql.com'], credentials: true };
     
+    const configurations = {
+        // Note: You may need sudo to run on port 443
+        production: { ssl: true, port: 443, hostname: 'api.yway.app' },
+        development: { ssl: false, port: 4000, hostname: 'localhost' },
+    };
+
+    const environment = process.env.NODE_ENV || 'production';
+    const config = configurations[environment];
+
     const app = express();
     const server = new ApolloServer({
         typeDefs,
@@ -32,7 +45,36 @@ async function startServer() {
 
     server.applyMiddleware({ app, cors: corsPolicy });
 
-    app.listen({ port: BACKEND_PORT }, () => { console.log(`Server listening on :${BACKEND_PORT}${server.graphqlPath}`); })
+    // Create the HTTPS or HTTP server, per configuration
+    let httpServer;
+    if (config.ssl) {
+      // Assumes certificates are in a .ssl folder off of the package root.
+      // Make sure these files are secured.
+      httpServer = https.createServer(
+        {
+            key: fs.readFileSync('/etc/letsencrypt/live/api.yway.app/privkey.pem'),
+            cert: fs.readFileSync('/etc/letsencrypt/live/api.yway.app/fullchain.pem'),
+            ca: fs.readFileSync('/etc/letsencrypt/live/api.yway.app/chain.pem')
+        },
+  
+        app,
+      );
+    } else {
+      httpServer = http.createServer(app);
+    }
+  
+    await new Promise(resolve =>
+      httpServer.listen({ port: config.port }, resolve)
+    );
+
+    console.log(
+      '🚀 Server ready at',
+      `http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}${
+        server.graphqlPath
+      }`
+    );  
+
+    // app.listen({ port: BACKEND_PORT }, () => { console.log(`Server listening on :${BACKEND_PORT}${server.graphqlPath}`); })
 }
 
 mongoose.connect(MONGO_URI)
